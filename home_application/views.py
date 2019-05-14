a6584f7c98fcc660de53377a3a05231f583b41ea
legion <1931330336@qq.com>
2019-05-14 20:52:12 +0800

    hello world

--- home_application/views.py
+++ home_application/views.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from blueapps.account.decorators import login_exempt
 
 
 # 开发框架中通过中间件默认是需要登录态的，如有不需要登录的，可添加装饰器login_exempt
 # 装饰器引入 from blueapps.account.decorators import login_exempt
+@login_exempt
 def home(request):
     """
     首页
